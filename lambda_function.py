5b438eb73322cc5cfe4337d054d96b9ce85fc1e3
AmandaQuinto <mandaquinto@gmail.com>
2020-08-21 19:08:40 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Twilio Informations #
-sid = 'AC85077cda3f33094e08c1b629822db34c'
-auth_token = 'c697671c0ba463b507d402619b7683c3'
-number = '+14155238886'
+sid = ''
+auth_token = ''
+number = '+'
 
 
 def msg_sender():
     whatsapp_client = Client(sid, auth_token)
     user = 'amanda'
-    user_number = '+5511987814764'
+    user_number = '+'
 
     msg = whatsapp_client.messages.create(
         body=f'Olá, {key}. Funcionou esta porra!',
